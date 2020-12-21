fb4dbea5327269baa9bfbc0df930ac777fcf808c
Davide Lunardon <dadolun@localhost.localdomain>
2020-12-21 11:20:41 +0100

    Leave installSchema, should work also into Magento2 previous versions.

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -11,17 +11,17 @@ use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
 /**
- * @FIXME replace with db_schema.xml
  * Class InstallSchema
  * @package Sendinblue\Sendinblue\Setup
  */
 class InstallSchema implements InstallSchemaInterface
 {
-    /**
-     * {@inheritdoc}
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     */
 
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @throws \Zend_Db_Exception
+     */
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $installer = $setup;
@@ -36,37 +36,53 @@ class InstallSchema implements InstallSchemaInterface
             'sendinblue_country_code_id',
             \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
             null,
-            ['identity' => true, 'nullable' => false, 'primary' => true],
+            [
+                'identity' => true,
+                'nullable' => false,
+                'primary' => true
+            ],
             'Country Telphone Code'
         )->addColumn(
             'iso_code',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
             255,
-            ['nullable' => false],
+            [
+                'nullable' => false
+            ],
             'iso code'
         )->addColumn(
             'country_prefix',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
             255,
-            ['nullable' => false],
+            [
+                'nullable' => false
+            ],
             'country prefix'
         )->addColumn(
             'status',
             \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
             null,
-            ['nullable' => false, 'default' => '1'],
+            [
+                'nullable' => false, 'default' => '1'
+            ],
             'Is Active'
         )->addColumn(
             'created_time',
             \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
             null,
-            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            [
+                'nullable' => false,
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT
+            ],
             'Creation Time'
         )->addColumn(
             'update_time',
             \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
             null,
-            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+            [
+                'nullable' => false,
+                'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE
+            ],
             'Modification Time'
         );
         $installer->getConnection()->createTable($table);
